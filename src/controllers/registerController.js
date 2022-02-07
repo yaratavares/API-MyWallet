1eb9a474f4bf36d49458a86dcbfaa3194cfa5b34
yaratavares <yaracriistavares@gmail.com>
2022-02-07 16:37:16 -0300

    bonus: update register

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -35,7 +35,6 @@ export async function deleteRegister(req, res) {
   const { id } = req.params;
   const { user } = res.locals;
 
-  console.log(user);
   if (id) {
     try {
       const register = await db
@@ -44,6 +43,37 @@ export async function deleteRegister(req, res) {
 
       if (register) {
         await db.collection("registers").deleteOne({ _id: new ObjectId(id) });
+        res.send(200);
+      } else {
+        res.sendStatus(400);
+      }
+    } catch {
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+}
+
+export async function updateRegister(req, res) {
+  const { id } = req.params;
+  const { user } = res.locals;
+  const register = req.body;
+
+  console.log(user);
+  if (id) {
+    try {
+      const registerExist = await db
+        .collection("registers")
+        .findOne({ _id: new ObjectId(id), userId: user._id });
+
+      if (registerExist) {
+        await db
+          .collection("registers")
+          .updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { ...register, userId: user._id } }
+          );
         res.send(200);
       } else {
         res.sendStatus(400);
--- src/routes/registerRouter.js
+++ src/routes/registerRouter.js
@@ -3,6 +3,7 @@ import {
   deleteRegister,
   getRegister,
   postRegister,
+  updateRegister,
 } from "../controllers/registerController.js";
 import registerValideMiddleware from "../middlewares/registerValideMiddleware.js";
 import tokenValidationMiddleware from "../middlewares/tokenValideMiddleware.js";
@@ -13,5 +14,6 @@ registerRouter.use(tokenValidationMiddleware);
 registerRouter.get("/registers", getRegister);
 registerRouter.post("/registers", registerValideMiddleware, postRegister);
 registerRouter.delete("/registers/:id", deleteRegister);
+registerRouter.put("/registers/:id", updateRegister);
 
 export default registerRouter;
