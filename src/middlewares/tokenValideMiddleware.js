517d3b247715783faf4f7636bbce6f9ddd5c8cc5
yaratavares <yaracriistavares@gmail.com>
2022-02-19 15:59:23 -0300

    feat: update registry

--- src/middlewares/tokenValideMiddleware.js
+++ src/middlewares/tokenValideMiddleware.js
@@ -1,11 +1,16 @@
 import db from "../db.js";
+import jwt from "jsonwebtoken";
 
 export default async function tokenValidationMiddleware(req, res, next) {
   const { authorization } = req.headers;
 
   const token = authorization?.replace("Bearer ", "");
+  const keySecret = process.env.JWT_SECRET;
 
-  if (!token) {
+  try {
+    const dados = jwt.verify(token, keySecret);
+    console.log(dados);
+  } catch {
     return res.sendStatus(401);
   }
 
